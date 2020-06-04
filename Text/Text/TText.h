9baf253b7722fa797cf4c46e60a43cb7048c380b
dashachernyh <dasha.chernyh.@mail.ru>
2020-06-04 17:15:22 +0300

    finish_ver

--- Text/Text/TText.h
+++ Text/Text/TText.h
@@ -4,34 +4,42 @@
 #include"TStack.h"
 #include<string>
 using namespace std;
+
 class TText
 {
-private:
-	TTextLink *pFirst, *pCurr;
-	TStack <TTextLink*> Stack;
+protected:
+	TTextLink *pFirst;   //указатель на первое звено
+	TTextLink *pCurr;    //указатель на текущее
+	TStack <TTextLink*> Stack;  //стек для итератора
+
+	void WriteRec(ofstream &ofs, TTextLink *p);   //вывод
+	TTextLink* ReadRec(ifstream &ifs);   //ввод
 public:
-	TText();
-	~TText() {};
-	TTextLink* GetpCurr();
-	void GoFirstLink();   //перейти на первую строку
+	TText();  //конструктор
+	~TText() {};  //деструктор
+	//доступ
+	TTextLink* GetpCurr();  //получить текущую запись
+	void SetLink(string str1); //записать строку на текущую поз
+	string GetLink();    //получить значение текущей строки
+	//навигация
+	void GoFirstLink();  //перейти на первую строку
 	void GoNextLink();   //перейти на следующий раздел в том же уровне
 	void GoDownLink();   //перейти на следующий подуровень
 	void GoPrevLink();   //вернутся на предыдущую строку
-	void SetLink(string str1);   //записать строку на текущую поз
-	string GetLink();    //получить текущую строку
+	//модификация
 	void InsNextLine(string str1);   //вставить следующую строку в этом же уровне
 	void InsNextSect(string str1);   //вставить раздел в том же уровне
 	void InsDownLine(string str1);   //вставить строку в том же подуровне
 	void InsDownSect(string str1);   //вставить раздел в том же подуровне
 	void DelNext();   //удаление строки на том же уровне
 	void DelDown();   //удаление раздела на том же уровне
-	void Read(string fn);   //ввод текста из файла
-	TTextLink* ReadRec(ifstream &ifs);   //ввод
-	void Write(string fn);   //вывод текста в файл 
-	void WriteRec(ofstream &ofs, TTextLink *p);   //вывод
 	//итераторы
 	int Reset();   //установить на первую запись
 	int IsEnd();   //конец ?
 	int GoNext();   //переход к след записи
-	void Print(TTextLink *p,TTextLink *curr);
+	//работа с файлом
+	void Read(string fn);   //ввод текста из файла
+	void Write(string fn);   //вывод текста в файл 
+	//печать
+	void Print(TTextLink *p, TTextLink *curr);  //печать на консоль
 };
--- Text/Text/TTextLink.h
+++ Text/Text/TTextLink.h
@@ -17,21 +17,23 @@ struct TTextMem
 class TTextLink
 {
 protected:
-	
-	static TTextMem MemHeader;
+	static TTextMem MemHeader;  //система управления памятью 
+	TTextLink *pNext, *pDown;   // указатели тек. звена на след уровень и подуровень
+	char str[81];               //поле для хранение строки текста
 public:
-	TTextLink *pNext, *pDown;
-	char str[81];
-	TTextLink(const char *_str = NULL, TTextLink *_pNext = NULL, TTextLink *_pDown = NULL);
-	~TTextLink() {}
-	static 	void InitMemSystem(int size = MemSize);
-	void* operator new(size_t size);
-	void operator delete (void *pMem);
-	static void PrintFreeLink();
-	static void MemCleaner(TText& txt);   //сборка муссора
+	TTextLink(const char *_str = NULL, TTextLink *_pNext = NULL, TTextLink *_pDown = NULL); //конструктор
+	~TTextLink() {}  //деструктор
+
+	static 	void InitMemSystem(int size = MemSize);  //инициализация памяти 
+	void* operator new(size_t size);                 //выделение памяти под звено
+	void operator delete (void *pMem);               //освобождение памяти под звено
+	static void PrintFreeLink();                     //печать свободных звеньев
+	static void MemCleaner(TText& txt);              //сборка муссора
+	
 	friend ostream& operator <<(ostream& os, const TTextLink& Link)
 	{
 		return os << Link.str;
 	}
+protected:
 	friend class TText;
 };
